5a80be6da7685f363796e24dcb00e8cbf73f16c5
FxTsuna <FxTsuna@outlook.fr>
2022-01-11 19:52:55 +0100

    Questionnaire et début tp1

--- chap-01/2-parrot.cpp
+++ chap-01/2-parrot.cpp
@@ -1,1 +1,15 @@
-// Implement the program here.
+#include <iostream>
+#include <string>
+
+int main()
+{
+    using namespace std;
+    for (int i = 0; i < 10; i++)
+    {
+        string word;
+        cin >> word;
+        cout << "craow" << word << "!" << std::endl;
+    }
+
+    return 0;
+}
--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main()
+int main(int argc, char** argv)
 {
-    int array[] = { 0, 1, 2, 3 };
-
-    for (int i = 0; i < 4; ++i)
+    if (argc < 2)
     {
-        std::cout << array[i] << std::endl;
+        std::cerr << "Expected argument" << std::endl;
+        return -1;
     }
+    int length = std::stoi(argv[1]);
+    if (length <= 0)
+    {
+        std::cerr << "Expected strictly positive value" << std::endl;
+        return -1;
+    }
+
+    std::cout << length << std::endl;
+
+    std::vector<int> array;
+
+    for (int i = 0; i < length; i++)
+    {
+        array.emplace_back(i + 1);
+    }
+
+    for (int value : array)
+    {
+        std::cout << value << std::endl;
+    }
+    
 
     return 0;
 }
